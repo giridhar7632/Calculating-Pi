e45b8a570a87ff4b6572f9fb3e1db8494ee3bd1f
giridhar7632 <giridhar.talla2002@gmail.com>
2021-02-04 20:51:39 +0530

    algorithm

---
+++ pi.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+# include <cstdlib>
+# include <ctime>
+# include <cmath>
+
+using namespace std;
+
+double computePi(const int n){
+  srand( time(0) );
+
+  int dartsInCircle=0;
+  for(int i=0; i<n; i++){
+    double x=rand()/(double)RAND_MAX, y=rand()/(double)RAND_MAX;
+    if(sqrt(x*x + y*y)<1){
+      ++dartsInCircle;
+    }
+  }
+  return dartsInCircle/static_cast<double>(n)*4;
+}
+
+int main(){
+  
+  int a = 100;
+  cout << "Pi = " <<  computePi(a);
+
+  return 0;
+}
